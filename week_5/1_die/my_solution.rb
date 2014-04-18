8ef3e3f8d91b4114252c363311a2caa1cf8f9a5a
C.J. Jameson <cjcjameson@gmail.com>
2014-04-18 11:40:51 -0700

    final solution to 1_die

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,28 +1,33 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: object creation call specifying how many sides
+# Output: two available calls, to ask how many sides it has, or roll the dice
+# Steps: Check during initialization that the die is valid
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+    
+    if sides <= 0
+    	raise ArgumentError.new("Must have a positive number of sides on the die")
+    end
+    @sides = sides
+
   end
   
   def sides
-    # code goes here
+    return @sides
   end
   
   def roll
-    # code goes here
+    return rand(sides)+1
   end
 end
 
@@ -36,10 +41,19 @@ end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-
-
+test_die = Die.new(6)
+p test_die.sides == 6
+p test_die.roll <= 6 && test_die.roll > 0
+p test_die.roll <= 6 && test_die.roll > 0
+p test_die.roll <= 6 && test_die.roll > 0
+p test_die.sides > 0
 
 
 
 
 # 5. Reflection 
+
+# I'm still getting comfortable with the @ notation within classes. I struggled
+# for a long time with an infinite loop call, and it was because sides was calling
+# sides was calling sides...haha. Good google searching eventually helped me out 
+# and I learned a bit about recursion in ruby too.
