8fe788794200f1a04c3a08ccbb1816c68cde182d
C.J. Jameson <cjcjameson@gmail.com>
2014-04-25 16:58:46 -0700

    final soln for 4_playlist

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -13,6 +13,54 @@
 
 # Initial Solution
 
+# class Song
+# 	attr_reader :title, :artist
+
+# 	def initialize(title, artist)
+# 		@title = title
+# 		@artist = artist
+# 	end
+
+# 	def play
+# 		p "Dooo Doooo Dah Dah #{@title} Dah Dah Dooooo... You recognize that sound, it's #{@artist}!"
+# 	end
+# end
+
+# class Playlist
+# 	def initialize(*songs)
+# 		@songs = songs.to_a
+# 	end
+
+# 	def add(*songs)
+# 		songs.each {|new_song| @songs << new_song }
+# 	end
+
+# 	def num_of_tracks
+# 		return @songs.length
+# 	end
+
+# 	def remove(*songs)
+# 		songs.each {|song| @songs.delete(song)}
+# 	end
+
+# 	def include?(song)
+# 		return @songs.include?(song)
+# 	end
+
+# 	def play_all
+# 		@songs.each {|song| song.play}
+# 	end
+
+# 	def list
+# 		@songs.each_with_index {|song,index|
+# 			p "#{index+1}. #{song.title} by #{song.artist}"
+# 		}
+# 	end
+# end
+
+
+# Refactored Solution
+
 class Song
 	attr_reader :title, :artist
 
@@ -40,7 +88,12 @@ class Playlist
 	end
 
 	def remove(*songs)
-		songs.each {|song| @songs.delete(song)}
+		songs.each {|song|
+			if include?(song) == false #added this error checking
+				raise ArgumentError, "this song isn't here--can't remove it"
+			end
+			@songs.delete(song)
+		}
 	end
 
 	def include?(song)
@@ -48,20 +101,22 @@ class Playlist
 	end
 
 	def play_all
+		if num_of_tracks>0 #added this check
+			raise ArgumentError, "nothing to play"
+		end
 		@songs.each {|song| song.play}
 	end
 
 	def list
+		if num_of_tracks>0 #added this check
+			raise ArgumentError, "nothing to list"
+		end
+		p "__Track List__"
 		@songs.each_with_index {|song,index|
 			p "#{index+1}. #{song.title} by #{song.artist}"
 		}
 	end
 end
-
-
-# Refactored Solution
-
-
 
 
 
@@ -84,8 +139,11 @@ p my_playlist.include?(lying_from_you) == true #renamed include? to be consisten
 my_playlist.play_all
 my_playlist.list #renamed this show rather than display
 
-
+empty_playlist = Playlist.new()
 
 
 
 # Reflection 
+
+# Good challenge to whip through quickly. The refactoring let me add more error checking.
+# I didn't see much to improve on in the driver code...was I missing somehing?
