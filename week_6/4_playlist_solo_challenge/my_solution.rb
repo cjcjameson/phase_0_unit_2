48409eeb86728fcd9a08acf294a52c0685ceeb12
C.J. Jameson <cjcjameson@gmail.com>
2014-04-25 16:51:38 -0700

    initial solution for 4_playlist

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -1,16 +1,62 @@
 # U2.W6: Create a Playlist from Driver Code
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself (cjcjameson).
 
 
 # Pseudocode
-
+# Declare a class Song that initializes with the song name and artist name
+# Song has the method play
+# Declare a class Playlist that initializes with any number of song objects, arranged into an instance variable array to hold them
+# Playlist has methods add, remove (taking a song object name), includes?(taking a song), play_all, and list
 
 
 # Initial Solution
 
+class Song
+	attr_reader :title, :artist
 
+	def initialize(title, artist)
+		@title = title
+		@artist = artist
+	end
+
+	def play
+		p "Dooo Doooo Dah Dah #{@title} Dah Dah Dooooo... You recognize that sound, it's #{@artist}!"
+	end
+end
+
+class Playlist
+	def initialize(*songs)
+		@songs = songs.to_a
+	end
+
+	def add(*songs)
+		songs.each {|new_song| @songs << new_song }
+	end
+
+	def num_of_tracks
+		return @songs.length
+	end
+
+	def remove(*songs)
+		songs.each {|song| @songs.delete(song)}
+	end
+
+	def include?(song)
+		return @songs.include?(song)
+	end
+
+	def play_all
+		@songs.each {|song| song.play}
+	end
+
+	def list
+		@songs.each_with_index {|song,index|
+			p "#{index+1}. #{song.title} by #{song.artist}"
+		}
+	end
+end
 
 
 # Refactored Solution
@@ -26,7 +72,7 @@ world_so_cold = Song.new("World So Cold", "Three Days Grace")
 going_under = Song.new("Going Under", "Evanescence")
  
 my_playlist = Playlist.new(one_by_one, world_so_cold, going_under)
- 
+
 lying_from_you = Song.new("Lying From You", "Linkin Park")
 angels = Song.new("Angels", "Within Temptation")
  
@@ -34,9 +80,9 @@ my_playlist.add(lying_from_you, angels)
 p my_playlist.num_of_tracks == 5
 going_under.play
 my_playlist.remove(angels)
-p my_playlist.includes?(lying_from_you) == true
+p my_playlist.include?(lying_from_you) == true #renamed include? to be consistent with standard ruby
 my_playlist.play_all
-my_playlist.display
+my_playlist.list #renamed this show rather than display
 
 
 
